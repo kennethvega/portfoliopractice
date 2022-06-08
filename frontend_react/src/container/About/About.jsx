7697230a9d52d3b8a46e2b9f2b36de3b16f5e758
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-06-08 13:58:08 +0800

    add a higher order component on header and about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,30 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./About.scss";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
 const About = () => {
-  const abouts = [
-    {
-      title: "Web Development",
-      description: "I am a good web developer.",
-      imgUrl: images.about01,
-    },
-    {
-      title: "Web Design",
-      description: "I am a good web developer.",
-      imgUrl: images.about02,
-    },
-    {
-      title: "UI/UX",
-      description: "I am a good web developer.",
-      imgUrl: images.about03,
-    },
-    {
-      title: "Web Animations",
-      description: "I am a good web developer.",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
       <h2 className="head-text">
@@ -41,7 +28,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -55,4 +42,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
