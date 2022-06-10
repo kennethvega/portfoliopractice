580517b921a89e55a144f189d3d6aa3e40d4be17
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-06-10 10:33:16 +0800

    footer done its the final code

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -18,6 +18,7 @@ const scaleVariants = {
 const Header = () => {
   return (
     <div className="app__header app__flex">
+  
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
